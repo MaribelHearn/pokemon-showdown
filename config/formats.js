MaribelHearn/pokemon-showdown

commit eef2f77606e82113e1aec54ca87c90160b9af0db

Inheritance: fix possible Donor Clause false positive

config/formats.js
@@ -482,7 +482,7 @@ exports.Formats = [
 				}
 				problems = this.validateSet(setCopy, teamHas);
 				if (!problems.length) legalPokemon.push(setCopy.species);
-				if (legalPokemon.length) break; // Remove if the FIXME? above gets fixed.
+				if (legalPokemon.length > 1) break; // Remove if the FIXME? above gets fixed.
 			}
 
 			if (problems.length && pokemonWithAbility.length > 1) {

commit 1e03b9e0240228b18b4ebf19747721a67dac90c3

Inheritance: fix major validator regression from eef2f77

config/formats.js
@@ -485,13 +485,13 @@ exports.Formats = [
 				if (legalPokemon.length > 1) break; // Remove if the FIXME? above gets fixed.
 			}
 
-			if (problems.length && pokemonWithAbility.length > 1) {
+			if (!legalPokemon.length && pokemonWithAbility.length > 1) {
 				return ["" + (set.name || set.species) + " has an illegal set with an ability from any of " + pokemonWithAbility.map(this.format.getPokemonName).join(", ")];
 			}
-			if (problems.length) {
+			if (!legalPokemon.length) {
 				problems.unshift("" + (set.name || set.species) + " has an illegal set with an ability from " + this.format.getPokemonName(pokemonWithAbility[0]));
+				return problems;
 			}
-			return problems;
 		}
 	},
 	{