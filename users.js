MaribelHearn/pokemon-showdown

commit be75115e49934cc30e0a70673832077166664ce4

Refactor Users#roomCount to Set
Users#roomCount, previously a null-prototype roomid:count Object,
is now Users#inRooms, a roomid Set.

The new name is "inRooms" rather than "rooms" mostly for ease of
greppability. I'm kind of tired of getting Rooms.rooms when
grepping for ".rooms"

This is the next step in the process of using Maps/Sets instead of
null-proto Objects for maps/sets we add/remove to/from a lot.

I tried to get rid of the room counter completely, but that made
iterating a user's rooms take tens of milliseconds rather than
tens of microseconds, which is unacceptably slow for how often we do
that.

Not actively tracking the number of connections in a room does not
change the asymptotic complexity of any function. It slows down
leaveRoom but doesn't change its asymptotic complexity (nor does
it slow it down in any noticeable way).

leaveRoom was, incidentally, kinda gnarly code which is now a lot
cleaner. Yay!

users.js
@@ -329,7 +329,7 @@ class User {
 		this.locked = false;
 		this.namelocked = false;
 		this.prevNames = Object.create(null);
-		this.roomCount = Object.create(null);
+		this.inRooms = new Set();
 
 		// Table of roomid:game
 		this.games = Object.create(null);
@@ -512,9 +512,9 @@ class User {
 		if (roomid) {
 			return Rooms(roomid).onUpdateIdentity(this);
 		}
-		for (let i in this.roomCount) {
-			Rooms(i).onUpdateIdentity(this);
-		}
+		this.inRooms.forEach(roomid => {
+			Rooms(roomid).onUpdateIdentity(this);
+		});
 	}
 	filterName(name) {
 		if (!Config.disablebasicnamefilter) {
@@ -777,15 +777,15 @@ class User {
 		for (let i in this.games) {
 			this.games[i].onRename(this, oldid, joining);
 		}
-		for (let i in this.roomCount) {
-			Rooms(i).onRename(this, oldid, joining);
-		}
+		this.inRooms.forEach(roomid => {
+			Rooms(roomid).onRename(this, oldid, joining);
+		});
 		return true;
 	}
 	merge(oldUser) {
-		for (let i in oldUser.roomCount) {
-			Rooms(i).onLeave(oldUser);
-		}
+		this.inRooms.forEach(roomid => {
+			Rooms(roomid).onLeave(oldUser);
+		});
 
 		if (this.locked === '#dnsbl' && !oldUser.locked) this.locked = false;
 		if (!this.locked && oldUser.locked === '#dnsbl') oldUser.locked = false;
@@ -797,7 +797,7 @@ class User {
 		for (let i = 0; i < oldUser.connections.length; i++) {
 			this.mergeConnection(oldUser.connections[i]);
 		}
-		oldUser.roomCount = {};
+		oldUser.inRooms.clear();
 		oldUser.connections = [];
 
 		this.s1 = oldUser.s1;
@@ -835,7 +835,7 @@ class User {
 		connection.user = this;
 		for (let i in connection.rooms) {
 			let room = connection.rooms[i];
-			if (!this.roomCount[i]) {
+			if (!this.inRooms.has(i)) {
 				if (room.bannedUsers && (this.userid in room.bannedUsers || this.autoconfirmed in room.bannedUsers)) {
 					// the connection was in a room that this user is banned from
 					room.bannedIps[connection.ip] = room.bannedUsers[this.userid];
@@ -844,9 +844,8 @@ class User {
 					continue;
 				}
 				room.onJoin(this, connection);
-				this.roomCount[i] = 0;
+				this.inRooms.add(i);
 			}
-			this.roomCount[i]++;
 			if (room.game && room.game.onUpdateConnection) {
 				room.game.onUpdateConnection(this, connection);
 			}
@@ -1002,14 +1001,12 @@ class User {
 		}
 		if (!this.connections.length) {
 			// cleanup
-			for (let i in this.roomCount) {
-				if (this.roomCount[i] > 0) {
-					// should never happen.
-					Monitor.debug('!! room miscount: ' + i + ' not left');
-					Rooms(i).onLeave(this);
-				}
-			}
-			this.roomCount = {};
+			this.inRooms.forEach(roomid => {
+				// should never happen.
+				Monitor.debug(`!! room miscount: ${roomid} not left`);
+				Rooms(roomid).onLeave(this);
+			});
+			this.inRooms.clear();
 			if (!this.named && !Object.keys(this.prevNames).length) {
 				// user never chose a name (and therefore never talked/battled)
 				// there's no need to keep track of this user, so we can
@@ -1035,13 +1032,11 @@ class User {
 			// should never happen
 			throw new Error("Failed to drop all connections for " + this.userid);
 		}
-		for (let i in this.roomCount) {
-			if (this.roomCount[i] > 0) {
-				// should never happen.
-				throw new Error("Room miscount: " + i + " not left for " + this.userid);
-			}
-		}
-		this.roomCount = {};
+		this.inRooms.forEach(roomid => {
+			// should never happen.
+			throw new Error(`Room miscount: ${roomid} not left for ${this.userid}`);
+		});
+		this.inRooms.clear();
 	}
 	getAlts(includeConfirmed, forPunishment) {
 		return this.getAltUsers(includeConfirmed, forPunishment).map(user => user.getLastName());
@@ -1145,11 +1140,9 @@ class User {
 			return true;
 		}
 		if (!connection.rooms[room.id]) {
-			if (!this.roomCount[room.id]) {
-				this.roomCount[room.id] = 1;
+			if (!this.inRooms.has(room.id)) {
+				this.inRooms.add(room.id);
 				room.onJoin(this, connection);
-			} else {
-				this.roomCount[room.id]++;
 			}
 			connection.joinRoom(room);
 			room.onConnect(this, connection);
@@ -1162,40 +1155,25 @@ class User {
 			// you can't leave the global room except while disconnecting
 			return false;
 		}
-		for (let i = 0; i < this.connections.length; i++) {
-			if (this.connections[i] === connection || !connection) {
-				if (this.connections[i].rooms[room.id]) {
-					if (this.roomCount[room.id]) {
-						this.roomCount[room.id]--;
-						if (!this.roomCount[room.id]) {
-							room.onLeave(this);
-							delete this.roomCount[room.id];
-						}
-					} else {
-						// should never happen
-						console.log('!! room miscount');
-					}
-					if (!this.connections[i]) {
-						// race condition? This should never happen, but it does.
-						fs.createWriteStream('logs/errors.txt', {'flags': 'a'}).on("open", fd => {
-							this.write("\nconnections = " + JSON.stringify(this.connections) + "\ni = " + i + "\n\n");
-							this.end();
-						});
-					} else {
-						this.connections[i].sendTo(room.id, '|deinit');
-						this.connections[i].leaveRoom(room);
-					}
-				}
-				if (connection) {
-					break;
-				}
-			}
+		if (!this.inRooms.has(room.id)) {
+			return false;
 		}
-		if (!connection && room.id in this.roomCount) {
-			// should also never happen
-			console.log('!! room miscount: ' + room.id + ' not left for ' + this.userid);
+		for (let i = 0; i < this.connections.length; i++) {
+			if (connection && this.connections[i] !== connection) continue;
+			if (this.connections[i].rooms[room.id]) {
+				this.connections[i].sendTo(room.id, '|deinit');
+				this.connections[i].leaveRoom(room);
+			}
+			if (connection) break;
+		}
+
+		let stillInRoom = false;
+		if (connection) {
+			stillInRoom = this.connections.some(connection => room.id in connection.rooms);
+		}
+		if (!stillInRoom) {
 			room.onLeave(this);
-			delete this.roomCount[room.id];
+			this.inRooms.delete(room.id);
 		}
 	}
 	prepBattle(formatid, type, connection) {

chat-plugins/info.js
@@ -50,13 +50,14 @@ exports.commands = {
 		let publicrooms = "";
 		let hiddenrooms = "";
 		let privaterooms = "";
-		for (let i in targetUser.roomCount) {
-			if (i === 'global') continue;
-			let targetRoom = Rooms.get(i);
+		targetUser.inRooms.forEach(roomid => {
+			if (roomid === 'global') return;
+			let targetRoom = Rooms.get(roomid);
 
-			let output = (targetRoom.auth && targetRoom.auth[targetUser.userid] ? targetRoom.auth[targetUser.userid] : '') + '<a href="/' + i + '">' + i + '</a>';
+			let authSymbol = (targetRoom.auth && targetRoom.auth[targetUser.userid] ? targetRoom.auth[targetUser.userid] : '');
+			let output = `${authSymbol}<a href="/${roomid}">${roomid}</a>`;
 			if (targetRoom.isPrivate === true) {
-				if (targetRoom.modjoin === '~') continue;
+				if (targetRoom.modjoin === '~') return;
 				if (privaterooms) privaterooms += " | ";
 				privaterooms += output;
 			} else if (targetRoom.isPrivate) {
@@ -66,7 +67,7 @@ exports.commands = {
 				if (publicrooms) publicrooms += " | ";
 				publicrooms += output;
 			}
-		}
+		});
 		buf += '<br />Rooms: ' + (publicrooms || '<em>(no public rooms)</em>');
 
 		if (!showAll) {

commands.js
@@ -1511,13 +1511,13 @@ exports.commands = {
 		}
 
 		// Destroy personal rooms of the locked user.
-		for (let i in targetUser.roomCount) {
-			if (i === 'global') continue;
-			let targetRoom = Rooms.get(i);
-			if (targetRoom.isPersonal && targetRoom.auth[userid] && targetRoom.auth[userid] === '#') {
+		targetUser.inRooms.forEach(roomid => {
+			if (roomid === 'global') return;
+			let targetRoom = Rooms.get(roomid);
+			if (targetRoom.isPersonal && targetRoom.auth[userid] === '#') {
 				targetRoom.destroy();
 			}
-		}
+		});
 
 		targetUser.popup("|modal|" + user.name + " has locked you from talking in chats, battles, and PMing regular users." + (target ? "\n\nReason: " + target : "") + "\n\nIf you feel that your lock was unjustified, you can still PM staff members (%, @, &, and ~) to discuss it" + (Config.appealurl ? " or you can appeal:\n" + Config.appealurl : ".") + "\n\nYour lock will expire in a few days.");
 
@@ -1570,13 +1570,13 @@ exports.commands = {
 		}
 
 		// Destroy personal rooms of the locked user.
-		for (let i in targetUser.roomCount) {
-			if (i === 'global') continue;
-			let targetRoom = Rooms.get(i);
-			if (targetRoom.isPersonal && targetRoom.auth[userid] && targetRoom.auth[userid] === '#') {
+		targetUser.inRooms.forEach(roomid => {
+			if (roomid === 'global') return;
+			let targetRoom = Rooms.get(roomid);
+			if (targetRoom.isPersonal && targetRoom.auth[userid] === '#') {
 				targetRoom.destroy();
 			}
-		}
+		});
 
 		targetUser.popup("|modal|" + user.name + " has locked you from talking in chats, battles, and PMing regular users for a week." + (target ? "\n\nReason: " + target : "") + "\n\nIf you feel that your lock was unjustified, you can still PM staff members (%, @, &, and ~) to discuss it" + (Config.appealurl ? " or you can appeal:\n" + Config.appealurl : ".") + "\n\nYour lock will expire in a few days.");
 
@@ -1656,13 +1656,13 @@ exports.commands = {
 		}
 
 		// Destroy personal rooms of the banned user.
-		for (let i in targetUser.roomCount) {
-			if (i === 'global') continue;
-			let targetRoom = Rooms.get(i);
-			if (targetRoom.isPersonal && targetRoom.auth[userid] && targetRoom.auth[userid] === '#') {
+		targetUser.inRooms.forEach(roomid => {
+			if (roomid === 'global') return;
+			let targetRoom = Rooms.get(roomid);
+			if (targetRoom.isPersonal && targetRoom.auth[userid] === '#') {
 				targetRoom.destroy();
 			}
-		}
+		});
 
 		targetUser.popup("|modal|" + user.name + " has banned you." + (target ? "\n\nReason: " + target : "") + (Config.appealurl ? "\n\nIf you feel that your ban was unjustified, you can appeal:\n" + Config.appealurl : "") + "\n\nYour ban will expire in a few days.");
 
@@ -3289,13 +3289,13 @@ exports.commands = {
 				return false;
 			}
 			let roomList = {};
-			for (let i in targetUser.roomCount) {
-				if (i === 'global') continue;
-				let targetRoom = Rooms.get(i);
-				if (!targetRoom) continue; // shouldn't happen
+			targetUser.inRooms.forEach(roomid => {
+				if (roomid === 'global') return;
+				let targetRoom = Rooms.get(roomid);
+				if (!targetRoom) return; // shouldn't happen
 				let roomData = {};
 				if (targetRoom.isPrivate) {
-					if (!(i in user.roomCount) && !(i in user.games)) continue;
+					if (!user.inRooms.has(roomid) && !(roomid in user.games)) return;
 					roomData.isPrivate = true;
 				}
 				if (targetRoom.battle) {
@@ -3303,8 +3303,8 @@ exports.commands = {
 					roomData.p1 = battle.p1 ? ' ' + battle.p1.name : '';
 					roomData.p2 = battle.p2 ? ' ' + battle.p2.name : '';
 				}
-				roomList[i] = roomData;
-			}
+				roomList[roomid] = roomData;
+			});
 			if (!targetUser.connected) roomList = false;
 			let userdetails = {
 				userid: targetUser.userid,

commit 57f49fd59b87af0ed82fdd56b3d929886c7c131e

Fix bug in user count
be75115e4 accidentally messed up how user merging works.

users.js
@@ -784,7 +784,7 @@ class User {
 		return true;
 	}
 	merge(oldUser) {
-		this.inRooms.forEach(roomid => {
+		oldUser.inRooms.forEach(roomid => {
 			Rooms(roomid).onLeave(oldUser);
 		});
 