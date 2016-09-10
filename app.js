MaribelHearn/pokemon-showdown

commit 1a1f64fe951442b072694954cff1d318cc254181

Remove .js extension from requires (#2725)

app.js
@@ -71,7 +71,7 @@ try {
  *********************************************************/
 
 try {
-	require.resolve('./config/config.js');
+	require.resolve('./config/config');
 } catch (err) {
 	if (err.code !== 'MODULE_NOT_FOUND') throw err; // should never happen
 
@@ -81,15 +81,16 @@ try {
 		fs.readFileSync(path.resolve(__dirname, 'config/config-example.js'))
 	);
 } finally {
-	global.Config = require('./config/config.js');
+	global.Config = require('./config/config');
 }
 
 if (Config.watchconfig) {
-	fs.watchFile(path.resolve(__dirname, 'config/config.js'), (curr, prev) => {
+	let configPath = require.resolve(__dirname, 'config/config');
+	fs.watchFile(configPath, (curr, prev) => {
 		if (curr.mtime <= prev.mtime) return;
 		try {
-			delete require.cache[require.resolve('./config/config.js')];
-			global.Config = require('./config/config.js');
+			delete require.cache[configPath];
+			global.Config = require('./config/config');
 			if (global.Users) Users.cacheGroupData();
 			console.log('Reloaded config/config.js');
 		} catch (e) {
@@ -102,38 +103,38 @@ if (Config.watchconfig) {
  * Set up most of our globals
  *********************************************************/
 
-global.Monitor = require('./monitor.js');
+global.Monitor = require('./monitor');
 
-global.Tools = require('./tools.js');
+global.Tools = require('./tools');
 global.toId = Tools.getId;
 
-global.LoginServer = require('./loginserver.js');
+global.LoginServer = require('./loginserver');
 
-global.Ladders = require(Config.remoteladder ? './ladders-remote.js' : './ladders.js');
+global.Ladders = require(Config.remoteladder ? './ladders-remote' : './ladders');
 
-global.Users = require('./users.js');
+global.Users = require('./users');
 
-global.Punishments = require('./punishments.js');
+global.Punishments = require('./punishments');
 
-global.Rooms = require('./rooms.js');
+global.Rooms = require('./rooms');
 
 delete process.send; // in case we're a child process
-global.Verifier = require('./verifier.js');
+global.Verifier = require('./verifier');
 Verifier.PM.spawn();
 
-global.CommandParser = require('./command-parser.js');
+global.CommandParser = require('./command-parser');
 
-global.Simulator = require('./simulator.js');
+global.Simulator = require('./simulator');
 
 global.Tournaments = require('./tournaments');
 
-global.Dnsbl = require('./dnsbl.js');
+global.Dnsbl = require('./dnsbl');
 Dnsbl.loadDatacenters();
 
 if (Config.crashguard) {
 	// graceful crash - allow current battles to finish before restarting
 	process.on('uncaughtException', err => {
-		let crashMessage = require('./crashlogger.js')(err, 'The main process');
+		let crashMessage = require('./crashlogger')(err, 'The main process');
 		if (crashMessage !== 'lockdown') return;
 		let stack = Tools.escapeHTML(err.stack).split("\n").slice(0, 2).join("<br />");
 		if (Rooms.lobby) {
@@ -152,7 +153,7 @@ if (Config.crashguard) {
  * Start networking processes to be connected to
  *********************************************************/
 
-global.Sockets = require('./sockets.js');
+global.Sockets = require('./sockets');
 
 exports.listen = function (port, bindAddress, workerCount) {
 	Sockets.listen(port, bindAddress, workerCount);
@@ -176,11 +177,11 @@ if (require.main === module) {
 Tools.includeFormats();
 Rooms.global.formatListText = Rooms.global.getFormatListText();
 
-global.TeamValidator = require('./team-validator.js');
+global.TeamValidator = require('./team-validator');
 TeamValidator.PM.spawn();
 
 /*********************************************************
  * Start up the REPL server
  *********************************************************/
 
-require('./repl.js').start('app', cmd => eval(cmd));
+require('./repl').start('app', cmd => eval(cmd));

battle-engine.js
@@ -4938,7 +4938,7 @@ Battle = (() => {
 			let side = this[slot];
 			if (!side) {
 				console.log('**** ' + slot + ' tried to leave before it was possible in ' + this.id);
-				require('./crashlogger.js')(new Error('**** ' + slot + ' tried to leave before it was possible in ' + this.id), 'A simulator process');
+				require('./crashlogger')(new Error('**** ' + slot + ' tried to leave before it was possible in ' + this.id), 'A simulator process');
 				return;
 			}
 

chat-plugins/datasearch.js
@@ -61,7 +61,7 @@ const PM = exports.PM = new ProcessManager({
 				result = null;
 			}
 		} catch (err) {
-			require('./../crashlogger.js')(err, 'A search query', data);
+			require('./../crashlogger')(err, 'A search query', data);
 			result = {error: "Sorry! Our search engine crashed on your query. We've been automatically notified and will fix this crash."};
 		}
 		return result;
@@ -242,24 +242,24 @@ exports.commands = {
 if (process.send && module === process.mainModule) {
 	// This is a child process!
 
-	global.Config = require('../config/config.js');
+	global.Config = require('../config/config');
 
 	if (Config.crashguard) {
 		process.on('uncaughtException', err => {
-			require('../crashlogger.js')(err, 'A dexsearch process', true);
+			require('../crashlogger')(err, 'A dexsearch process', true);
 		});
 	}
 
-	global.Tools = require('../tools.js');
+	global.Tools = require('../tools');
 	global.toId = Tools.getId;
 	Tools.includeData();
 	Tools.includeMods();
-	global.TeamValidator = require('../team-validator.js');
+	global.TeamValidator = require('../team-validator');
 
 	process.on('message', message => PM.onMessageDownstream(message));
 	process.on('disconnect', () => process.exit());
 
-	require('../repl.js').start('dexsearch', cmd => eval(cmd));
+	require('../repl').start('dexsearch', cmd => eval(cmd));
 } else if (!PM.maxProcesses) {
 	process.nextTick(() => Tools.includeMods());
 }

chat-plugins/mafia.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-let MafiaData = require('./mafia-data.js');
+let MafiaData = require('./mafia-data');
 
 const deadImage = '<img width="75" height="75" src="//play.pokemonshowdown.com/fx/mafia-dead.png" />';
 const meetingMsg = {town: 'The town has lynched a suspect!', mafia: 'The mafia strikes again!'};

command-parser.js
@@ -60,13 +60,13 @@ exports.multiLinePattern = {
  * Load command files
  *********************************************************/
 
-let baseCommands = exports.baseCommands = require('./commands.js').commands;
+let baseCommands = exports.baseCommands = require('./commands').commands;
 let commands = exports.commands = Object.assign({}, baseCommands);
 
 // Install plug-in commands
 
 // info always goes first so other plugins can shadow it
-Object.assign(commands, require('./chat-plugins/info.js').commands);
+Object.assign(commands, require('./chat-plugins/info').commands);
 
 for (let file of fs.readdirSync(path.resolve(__dirname, 'chat-plugins'))) {
 	if (file.substr(-3) !== '.js' || file === 'info.js') continue;
@@ -283,7 +283,7 @@ class CommandContext {
 		try {
 			result = commandHandler.call(this, this.target, this.room, this.user, this.connection, this.cmd, this.message);
 		} catch (err) {
-			if (require('./crashlogger.js')(err, 'A chat command', {
+			if (require('./crashlogger')(err, 'A chat command', {
 				user: this.user.name,
 				room: this.room.id,
 				message: this.message,

commands.js
@@ -2501,10 +2501,10 @@ exports.commands = {
 					}
 				}
 
-				CommandParser.uncacheTree('./command-parser.js');
-				delete require.cache[require.resolve('./commands.js')];
-				delete require.cache[require.resolve('./chat-plugins/info.js')];
-				global.CommandParser = require('./command-parser.js');
+				CommandParser.uncacheTree('./command-parser');
+				delete require.cache[require.resolve('./commands')];
+				delete require.cache[require.resolve('./chat-plugins/info')];
+				global.CommandParser = require('./command-parser');
 
 				let runningTournaments = Tournaments.tournaments;
 				CommandParser.uncacheTree('./tournaments');
@@ -2524,9 +2524,9 @@ exports.commands = {
 			} else if (target === 'formats') {
 				let toolsLoaded = !!Tools.isLoaded;
 				// uncache the tools.js dependency tree
-				CommandParser.uncacheTree('./tools.js');
+				CommandParser.uncacheTree('./tools');
 				// reload tools.js
-				global.Tools = require('./tools.js')[toolsLoaded ? 'includeData' : 'includeFormats'](); // note: this will lock up the server for a few seconds
+				global.Tools = require('./tools')[toolsLoaded ? 'includeData' : 'includeFormats'](); // note: this will lock up the server for a few seconds
 				// rebuild the formats list
 				Rooms.global.formatListText = Rooms.global.getFormatListText();
 				// respawn validator processes
@@ -2539,15 +2539,15 @@ exports.commands = {
 				return this.sendReply("Formats have been hotpatched.");
 			} else if (target === 'loginserver') {
 				fs.unwatchFile('./config/custom.css');
-				CommandParser.uncacheTree('./loginserver.js');
-				global.LoginServer = require('./loginserver.js');
+				CommandParser.uncacheTree('./loginserver');
+				global.LoginServer = require('./loginserver');
 				return this.sendReply("The login server has been hotpatched. New login server requests will use the new code.");
 			} else if (target === 'learnsets' || target === 'validator') {
 				TeamValidator.PM.respawn();
 				return this.sendReply("The team validator has been hotpatched. Any battles started after now will have teams be validated according to the new code.");
 			} else if (target === 'punishments') {
-				delete require.cache[require.resolve('./punishments.js')];
-				global.Punishments = require('./punishments.js');
+				delete require.cache[require.resolve('./punishments')];
+				global.Punishments = require('./punishments');
 				return this.sendReply("Punishments have been hotpatched.");
 			} else if (target === 'dnsbl') {
 				Dnsbl.loadDatacenters();

data/scripts.js
@@ -1159,7 +1159,7 @@ exports.BattleScripts = {
 			template = this.getTemplate('unown');
 
 			let err = new Error('Template incompatible with random battles: ' + species);
-			require('../crashlogger.js')(err, 'The randbat set generator');
+			require('../crashlogger')(err, 'The randbat set generator');
 		}
 
 		if (typeof teamDetails !== 'object') teamDetails = {megaCount: teamDetails};
@@ -2433,7 +2433,7 @@ exports.BattleScripts = {
 			template = this.getTemplate('unown');
 
 			let err = new Error('Template incompatible with random battles: ' + species);
-			require('../crashlogger.js')(err, 'The doubles randbat set generator');
+			require('../crashlogger')(err, 'The doubles randbat set generator');
 		}
 
 		if (typeof teamDetails !== 'object') teamDetails = {megaCount: teamDetails};

mods/gen5/scripts.js
@@ -11,7 +11,7 @@ exports.BattleScripts = {
 			template = this.getTemplate('unown');
 
 			let err = new Error('Template incompatible with random battles: ' + name);
-			require('./../../crashlogger.js')(err, 'The randbat set generator');
+			require('./../../crashlogger')(err, 'The randbat set generator');
 		}
 
 		if (template.battleOnly) name = template.baseSpecies;

commit 57e8d7be3fbe4fdf07dd763f29e738289e85ff84

Fix watchconfig
Broken in 1a1f64fe95

app.js
@@ -85,7 +85,7 @@ try {
 }
 
 if (Config.watchconfig) {
-	let configPath = require.resolve(__dirname, 'config/config');
+	let configPath = require.resolve('./config/config');
 	fs.watchFile(configPath, (curr, prev) => {
 		if (curr.mtime <= prev.mtime) return;
 		try {