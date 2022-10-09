25cd49424f0257b428131d6678f5c88afc80e10f
Maribel Hearn <maribelhearn@disroot.org>
2022-10-09 23:06:26 +0200

    Test Arrow/Beam

--- server/chat-plugins/datasearch.ts
+++ server/chat-plugins/datasearch.ts
@@ -1207,7 +1207,7 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 	const allFlags = [
 		'bypasssub', 'bite', 'bullet', 'charge', 'contact', 'dance', 'defrost', 'gravity', 'highcrit', 'mirror',
 		'multihit', 'ohko', 'powder', 'protect', 'pulse', 'punch', 'recharge', 'reflectable', 'secondary',
-		'snatch', 'sound', 'zmove', 'maxmove', 'gmaxmove', 'protection',
+		'snatch', 'sound', 'zmove', 'maxmove', 'gmaxmove', 'protection', 'arrow', 'beam'
 	];
 	const allStatus = ['psn', 'tox', 'brn', 'par', 'frz', 'slp'];
 	const allVolatileStatus = ['flinch', 'confusion', 'partiallytrapped'];
