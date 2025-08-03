3f6493a62c69a82dd2201fdb0bfedc4568dfd7ee
Maribel Hearn <maribelhearn@disroot.org>
2025-08-03 19:43:35 +0200

    Telemetry again

--- data/moves.ts
+++ data/moves.ts
@@ -21369,10 +21369,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (unrevealedMoves.length > 0) {
 				const randInt = this.random(unrevealedMoves.length);
 				const move = unrevealedMoves[randInt];
+				target.addVolatile('telemetry', source, move);
 				this.add('-activate', target, 'Telemetry', move.name);
 			}
 		},
-		volatileStatus: 'telemetry',
 		secondary: null,
 		target: "normal",
 		type: "Steel",
