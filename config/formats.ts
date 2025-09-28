63dab0bf2ad8748389093d897cb5eb6ec61690d1
Maribel Hearn <maribelhearn@disroot.org>
2025-09-28 23:42:59 +0200

    Fix Loser's Game

--- config/formats.ts
+++ config/formats.ts
@@ -150,7 +150,7 @@ export const Formats: FormatList = [
 		mod: 'gen8',
 		ruleset: ['Fundex OMs', 'Sleep Clause Mod', '!OHKO Clause', 'Picked Team Size = 6', 'Adjust Level = 100'],
 		banlist: [
-			'Fundex Uber', 'All Pokemon', 'Zeeky H. Bomb', 'Infiltrator', 'Magic Guard', 'Choice Scarf', 'Explosion', 'Final Gambit', 'Healing Wish', 'Lunar Dance', 'Magic Room', 'Memento', 'Misty Explosion', 'Self-Destruct', 'Catnarok', 'System Crash', 'Negative Zone',
+			'All Pokemon', 'Zeeky H. Bomb', 'Infiltrator', 'Magic Guard', 'Choice Scarf', 'Explosion', 'Final Gambit', 'Healing Wish', 'Lunar Dance', 'Magic Room', 'Memento', 'Misty Explosion', 'Self-Destruct', 'Catnarok', 'System Crash', 'Negative Zone',
 		],
 		unbanlist: ['Fundex'],
 		onValidateTeam(team) {
